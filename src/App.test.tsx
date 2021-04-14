c301caba82dd1725d478ccd83d243ee187ab0383
Domenico Gemoli <domenicogemoli@gmail.com>
2021-04-14 14:47:58 +0200

    Try and add ts-jest for type checking in tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function sum(a: number, b: number) {
+  return a + b;
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("sum", () => {
+  expect(sum(1, "2")).toEqual("12");
 });
